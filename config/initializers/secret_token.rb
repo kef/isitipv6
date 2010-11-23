d9f0e23e05192ba3889557a279a5211ed3d52551
Paul O'Keeffe <paul.okeeffe@columina.com>
2010-11-23 16:18:07 +1000

    Created as a Rails 3.0 web app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isitipv6::Application.config.secret_token = 'dbda0ee389863978350b345b2a8d4d36bb281806722bd21dbbda3468a29c38b6ec38d97aa550f028c8576531e097196226c5e2450a5123b88e5d385a60b8f84e'
